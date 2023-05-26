1b48adbdc5f4959bce6f6ba5845fbddde6f9c40c
Василь Свідрік <svidryk@corp2.eu>
2023-05-26 13:31:17 +0300

    edit3 setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myproject',
+    version='1.0.0',
+    description='Description of my project',
+    author='Your Name',
+    author_email='yourname@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'k2test'
+    ],
+)
