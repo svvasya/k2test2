b07d64577868f9304291a1ed0c80b983bb32b77f
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-27 11:28:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='myproject',
+    name='k2test',
     version='1.0.0',
     description='Description of my project',
     author='Your Name',
     author_email='yourname@example.com',
     packages=find_packages(),
-    package_data={'views.py'},  # Додано 'views.py' як файл для копіювання
+    package_data={'k2test':['views.py']},  # Додано 'views.py' як файл для копіювання
     install_requires=[
 
     ],
