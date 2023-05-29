004c1902cdd7cb81861062ed44f9d84649e6be8b
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-29 15:39:14 +0300

    Update views.py k2root

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request
 from flask_login import login_user, current_user,  logout_user, login_required
-from k2.config import app
+from k2root.config import app
 @app.route('/new')
 @login_required
 def new_components():
