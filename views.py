a071c95915a360c7b00b2e7504252b48467d1b08
svidryk <102587970+svvasya@users.noreply.github.com>
2023-05-30 17:30:05 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, request
-from flask_login import login_user, current_user,  logout_user, login_required
-from k2root.config import app
-@app.route('/new')
-@login_required
-def new_components():
+from flask import Blueprint
+
+k2test_bp = Blueprint('k2test', __name__)
+
+@k2test_bp.route('/new')
+def new_component():
     return 'нова встановлена компонента'
